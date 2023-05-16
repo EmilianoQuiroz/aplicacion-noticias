50f49d4d6665574dc1500a9d641574327a4b035b
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-05-16 00:01:28 -0300

    Consulta de noticias segun su categoria

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import { useState, useEffect, createContext} from 'react'
 
 const NoticiasContext = createContext()
@@ -5,6 +6,20 @@ const NoticiasContext = createContext()
 const NoticiasProvider = ({children}) => {
 
     const [categoria, setCategoria] = useState('general')
+    const [noticias, setNoticias] = useState([])
+
+    useEffect(() => {
+        
+        const consultarAPI = async () => {
+            const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apikey=${import.meta.env.VITE_API_KEY}`
+
+            const { data } = await axios(url)
+
+            setNoticias(data.articles)
+        }
+        consultarAPI()
+
+    }, [categoria])
 
     const handleChangeCategoria = e => {
         setCategoria(e.target.value)
