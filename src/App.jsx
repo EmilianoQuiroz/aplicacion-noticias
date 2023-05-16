327b47ad3fa3ed8f1a50006a8b966c56ad84921e
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-05-15 23:27:52 -0300

    Creando el Context

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,43 @@
 import {Container, Grid, Typography} from '@mui/material';
 // Importamos los componentes que se van a mostrar
 import Formulario from './components/Formulario';
+import { NoticiasProvider } from './context/NoticiasProvider';
 
 function App() {
 
   return (
-    <Container>
-      <header>
-        {/**
-        * Con el componente Typography manejamos las tipografias en MUI
-        * Con Variant podemos decir que apariencia va a tener, mientras
-        * que Component va a ser el componente que se va a mostrar
-        * SX es un prop que podemos usar para definir estilos personalizados
-        */}
-        <Typography 
-        align='center' 
-        marginY={5}
-        component="h1"
-        variant="h3"
+    <NoticiasProvider>
+      <Container>
+        <header>
+          {/**
+          * Con el componente Typography manejamos las tipografias en MUI
+          * Con Variant podemos decir que apariencia va a tener, mientras
+          * que Component va a ser el componente que se va a mostrar
+          * SX es un prop que podemos usar para definir estilos personalizados
+          */}
+          <Typography 
+          align='center' 
+          marginY={5}
+          component="h1"
+          variant="h3"
+          >
+            Buscador de Noticias
+          </Typography>
+        </header>
+        
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
         >
-          Buscador de Noticias
-        </Typography>
-      </header>
-      
-      <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Grid item xs={12} md={6} lg={4}>
-          <Formulario />
+          <Grid item xs={12} md={6} lg={4}>
+            <Formulario />
+          </Grid>
         </Grid>
-      </Grid>
 
-    </Container>
+      </Container>
+    </NoticiasProvider>
   )
 }
 
