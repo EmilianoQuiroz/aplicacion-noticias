557b8d1755cd1a6fbf7cf1d17de6eefd0be39a6d
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-05-15 22:36:44 -0300

    Primeros componentes en Material UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+// Importamos componentes de MUI
+import {Container, Grid, Typography} from '@mui/material';
+
 function App() {
 
   return (
-    <>
-      <h1>App de Noticias</h1>
-    </>
+    <Container>
+      <header>
+        {/**
+        * Con el componente Typography manejamos las tipografias en MUI
+        * Con Variant podemos decir que apariencia va a tener, mientras
+        * que Component va a ser el componente que se va a mostrar
+        * SX es un prop que podemos usar para definir estilos personalizados
+        */}
+        <Typography 
+        align='center' 
+        marginY={5}
+        component="h1"
+        variant="h3"
+        >
+          Buscador de Noticias
+        </Typography>
+      </header>
+    </Container>
   )
 }
 
